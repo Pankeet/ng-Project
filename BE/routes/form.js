477ef3d2cba0e083ed28c8dd9b2164bf669d4d13
Pankeet <pankeet04@gmail.com>
2025-04-12 15:50:26 +0530

    Version 1.2.1

--- BE/routes/form.js
+++ BE/routes/form.js
@@ -6,39 +6,43 @@ const studentForm = Router();
 studentForm.post('/submit', async function(req, res){
         
     try{
-    const user = req.body;
+        const user = req.body;
 
-    let a=await StudentModel.findOne({email});
-    let b=await StudentModel.findOne({phone});
+        let a=await StudentModel.findOne({
+            email : user.email , 
+             });
+        let b=await StudentModel.findOne({
+            phone : user.phone , 
+            });
 
-    if(a)
-    {
-        return res.status(400).json({
-            success:false,
-            message:"Email already exists"
+        if(a)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"Email already exists"
+            })
+        }
+
+        if(b)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"Phone number already exists"
+            })
+        }
+
+        await StudentModel.create({
+            firstname: user.firstName,
+            lastname: user.lastName,
+            email: user.email,
+            password: user.password,
+            phone: user.phone,
+            dob : user.age 
         })
-    }
-
-    if(b)
-    {
-        return res.status(400).json({
-            success:false,
-            message:"Phone number already exists"
-        })
-    }
-
-    await StudentModel.create({
-        firstname: user.firstName,
-        lastname: user.lastName,
-        email: user.email,
-        password: user.password,
-        phone: user.phone,
-        dob : user.age 
-    })
-   
-        res.json({
-            message : "User Detailed Recorded Successfully"
-        })
+    
+            res.json({
+                message : "User Detailed Recorded Successfully" ,
+            })
     }
 
 
